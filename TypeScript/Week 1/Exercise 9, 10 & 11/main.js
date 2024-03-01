f6bcf1bbb12fe753d014ed15005ae8a60594f233
zahir9048 <zahir9048@gmail.com>
2024-03-01 17:31:10 +0500

    c

---
+++ TypeScript/Week 1/Exercise 9, 10 & 11/main.js
@@ -0,0 +1,14 @@
+//Exercise 9
+console.log("\nExercise 9");
+var num = 9;
+console.log("Number revealed: " + num + "\n");
+//Exercise 10
+// Zahir Khan 1st March, 2024
+console.log("\nExercise 10");
+console.log("Number revealed: " + num + "\n"); //This program just prints the number stored in the variable: num
+//Exercise 11
+console.log("\nExercise 11");
+var friends = ["adil", "ramish", "moiz"];
+console.log(friends[0]);
+console.log(friends[1]);
+console.log(friends[2]);
---
+++ TypeScript/Week 1/Exercise 9, 10 & 11/main.ts
@@ -0,0 +1,21 @@
+
+//Exercise 9
+console.log("\nExercise 9");
+let num = 9;
+console.log("Number revealed: " + num + "\n");
+
+
+
+//Exercise 10
+// Zahir Khan 1st March, 2024
+console.log("\nExercise 10");
+console.log("Number revealed: " + num + "\n"); //This program just prints the number stored in the variable: num
+
+
+//Exercise 11
+
+console.log("\nExercise 11");
+let names = ["adil", "ramish", "moiz"];
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
