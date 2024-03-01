a6fd920aeaee5ca0594b4f6f7fe82e79b8d66349
zahir9048 <zahir9048@gmail.com>
2024-03-01 16:07:24 +0500

    commit

---
+++ TypeScript/Week 1/Exercise 4/main.js
@@ -0,0 +1,4 @@
+"use strict";
+let author = "Imran Khan";
+let quote = "You only lose when you give up.";
+console.log(author + " said: " + quote);
---
+++ TypeScript/Week 1/Exercise 4/main.ts
@@ -0,0 +1,5 @@
+
+
+let author = "Imran Khan";
+let quote = "You only lose when you give up.";
+console.log(author + " said: " + quote);
