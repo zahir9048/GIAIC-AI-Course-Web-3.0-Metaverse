cf0530f939df6ed5ca13e172846bf15eb67f778d
zahir9048 <zahir9048@gmail.com>
2024-03-01 16:52:30 +0500

    commit

--- TypeScript/Week 1/Exercise 6,7 & 9/main.ts
+++ TypeScript/Week 1/Exercise 6,7 & 9/main.ts
@@ -9,3 +9,14 @@ console.log("addition: " + (5 + 3));
 console.log("subtraction: " + (10 - 2));
 console.log("multiplication: " + (4 * 2));
 console.log("division: " + (16 / 2));
+
+//Exercise 8
+console.log("\nExercise 8");
+console.log(7 + 1);
+console.log(2 + 6);
+console.log(4 + 4);
+console.log(5 + 3);
+
+
+
+
