cc0d48fbbbf7698aedbdb5fb26490f1d78b513d1
zahir9048 <zahir9048@gmail.com>
2024-03-02 14:10:02 +0500

    commit

---
+++ TypeScript/Week 1/Exercise 12, 13 & 14/main.js
@@ -0,0 +1,19 @@
+//Exercise 12
+console.log("\nExercise 12");
+var names = ["adil", "ramish", "moiz"];
+console.log("Hello " + names[0] + ", how are you?");
+console.log("Hello " + names[1] + ", how are you?");
+console.log("Hello " + names[2] + ", how are you?");
+//Exercise 13
+// Zahir Khan 1st March, 2024
+console.log("\nExercise 13");
+var list = ["Honda Bike", "Toyata Car", "Hyundai Van"];
+console.log("I would like to own a " + list[0]);
+console.log("I would like to own a " + list[1]);
+console.log("I would like to own a " + list[2]);
+//Exercise 14
+console.log("\nExercise 14");
+var guests = ["Asim", "Adil", "Ramish"];
+console.log("I would like to invite " + guests[0] + " to dinner.");
+console.log("I would like to invite " + guests[1] + " to dinner.");
+console.log("I would like to invite " + guests[2] + " to dinner.");
