e94858768ac941dc2c45e99dac78d04e7cbbab58
zahir9048 <zahir9048@gmail.com>
2024-03-03 21:02:59 +0500

    commit

---
+++ TypeScript/Week 1/Exercise 19/main.js
@@ -0,0 +1,4 @@
+//Exercise 19
+console.log("\nExercise 19");
+var guests = ["Asim", "Umer", "Adil", "Ramish"];
+console.log("Total number of guests invited for dinner: " + guests.length);
---
+++ TypeScript/Week 1/Exercise 19/main.ts
@@ -0,0 +1,5 @@
+//Exercise 19
+console.log("\nExercise 19");
+let guests: string[] = ["Asim", "Umer", "Adil", "Ramish"];
+
+console.log("Total number of guests invited for dinner: "+ guests.length);
