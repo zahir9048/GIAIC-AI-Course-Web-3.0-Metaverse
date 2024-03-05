abaeb9f0705726c5b05b76a00b1ba84590d18788
zahir9048 <zahir9048@gmail.com>
2024-03-06 03:19:36 +0500

    commit

---
+++ TypeScript/Week 1/Exercise 21/main.ts
@@ -0,0 +1,19 @@
+//Exercise 21
+console.log("\nExercise 21");
+console.log("Objects in TypeScript JS\n");
+
+var person = {   //thats how we create obejcts in TypeScript
+    firstname: "Zahir",   //storing variables inside object
+    lastname: "Khan",
+    dateOfBirth: "26-dec-2000",
+    age: 23,
+    weekdays: ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'] //storing array inside object
+};
+//access the object values 
+console.log("First Name: " + person.firstname);
+console.log("Last Name: " + person.lastname);
+console.log("Date Of Birth: " + person.dateOfBirth);
+console.log("Age: " + person.age);
+console.log("Weekday: " + person.weekdays[0]);
+console.log("");
+
---
+++ TypeScript/Week 1/Exercise 22/main.ts
@@ -0,0 +1,13 @@
+//Exercise 22
+console.log("\nExercise 22");
+console.log("Array Index Error\n");
+
+let arr: string[] = ['apple', 'banana', 'cherry'];
+//Index Mapping:    0   ,    1    ,    2
+
+console.log(arr[3]); // this will print undefined in output as last array index is 2
+
+// Note: Array index starts from 0
+
+
+console.log(arr[2]); //Now the last value will be printed in output
