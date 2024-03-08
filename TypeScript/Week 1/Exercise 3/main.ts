34f758ba1647b6d125ca043b7db2edfb7e4a462e
zahir9048 <zahir9048@gmail.com>
2024-03-08 16:43:55 +0500

    commit update

--- TypeScript/Week 1/Exercise 3/main.ts
+++ TypeScript/Week 1/Exercise 3/main.ts
@@ -5,5 +5,4 @@ console.log("Original Name: " + personName);
 console.log("Lower case: " + personName.toLowerCase());
 console.log("Upper case: " + personName.toUpperCase());
 
-let titlecaseName = personName.replace(/\b\w+/g, (word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase());
-console.log("Title case: " + titlecaseName);
+console.log("Titlecase: "+personName.charAt(0).toUpperCase() + personName.slice(1).toLowerCase());
