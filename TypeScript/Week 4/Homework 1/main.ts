078edb4616e704da5eb0148e7eaf21379cd8bec3
zahir9048 <78537600+zahir9048@users.noreply.github.com>
2024-03-12 06:54:37 +0500

    Update main.ts

--- TypeScript/Week 4/Homework 1/main.ts
+++ TypeScript/Week 4/Homework 1/main.ts
@@ -1,5 +1,5 @@
 
-//Homework slide 121
+//Homework slide 121: Different Operators
 let a: number = 5; 
 let b: number = 2;
 let c: number;
@@ -9,7 +9,7 @@ c = ++a + a++ + --b + b-- + a + b++ + b;
 console.log(c); //22
 
 
-//Homework slide 131
+//Homework slide 131: Calculate BMI Index
 let weightInKg = 60;
 let heightInMeter = 1.77;
 let bmi = weightInKg / (heightInMeter^2);
