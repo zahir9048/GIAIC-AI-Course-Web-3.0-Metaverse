68b739c65b2cc8585c2c640664688089014f1232
zahir9048 <zahir9048@gmail.com>
2024-03-23 16:41:51 +0500

    commit

--- TypeScript/Projects/number-guessing-game/index.js
+++ TypeScript/Projects/number-guessing-game/index.js
@@ -4,22 +4,30 @@ import inquirer from "inquirer";
 //2 User input for guessing number
 //3 Compare user input with computer generated number and show result
 const randomNumber = Math.floor(Math.random() * 10 + 1);
-while (true) {
+let tries = 5;
+let won = false;
+while (tries > 0) {
+    tries--;
     const answers = await inquirer.prompt([
         {
             name: "numberInput",
             type: "number",
-            message: "Please guess a number: ",
+            message: "Please guess a number (1-10): ",
         },
     ]);
     if (answers.numberInput === randomNumber) {
+        won = true;
         console.log("Congratulations! You guessed the right number.");
         break;
     }
     else if (answers.numberInput < randomNumber) {
-        console.log("Your guess is too low. Try again!");
+        console.log("Your guess is too low.");
     }
-    else {
-        console.log("Your guess is too high. Try again!");
+    else if (answers.numberInput > randomNumber) {
+        console.log("Your guess is too high.");
     }
+    console.log("You have " + tries + " tries left.\n");
+}
+if (tries == 0 && won == false) {
+    console.log("You Lost, You didnt the guess correctly in 5 tries.");
 }
--- TypeScript/Projects/number-guessing-game/index.ts
+++ TypeScript/Projects/number-guessing-game/index.ts
@@ -6,23 +6,35 @@ import inquirer from "inquirer";
 //2 User input for guessing number
 //3 Compare user input with computer generated number and show result
 
-const randomNumber: number = Math.floor(Math.random() * 10 + 1); 
+const randomNumber: number = Math.floor(Math.random() * 10 + 1);
 
-while (true) {
+let tries: number = 5;
+let won: boolean = false;
+
+while (tries > 0) {
+  tries--;
+
   const answers = await inquirer.prompt([
     {
       name: "numberInput",
       type: "number",
-      message: "Please guess a number: ",
+      message: "Please guess a number (1-10): ",
     },
   ]);
 
   if (answers.numberInput === randomNumber) {
+    won = true;
     console.log("Congratulations! You guessed the right number.");
     break;
   } else if (answers.numberInput < randomNumber) {
-    console.log("Your guess is too low. Try again!");
-  } else {
-    console.log("Your guess is too high. Try again!");
+    console.log("Your guess is too low.");
+  } else if (answers.numberInput > randomNumber) {
+    console.log("Your guess is too high.");
   }
+
+  console.log("You have " + tries + " tries left.\n");
+}
+
+if (tries == 0 && won == false) {
+  console.log("You Lost, You didnt the guess correctly in 5 tries.");
 }
