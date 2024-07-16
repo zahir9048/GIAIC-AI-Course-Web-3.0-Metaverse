70562a33e6ceb2dcb8dd8740277d275e741e1d92
zahir9048 <zahir9048@gmail.com>
2024-07-16 17:40:39 +0500

    commit step 7a & 7b

---
+++ TypeScript/Sir Zia Repo Practice/Step 7a/main.ts
@@ -0,0 +1,41 @@
+// enum Days {
+//     // members
+//     Monday,    // values 
+//     Tuesday = 6,// 
+//     Wednesday = 8, // 
+//     Thursday = 9,
+//     Friday
+// }
+
+// console.log(Days.Monday); // 0,  Accessing value
+// console.log(Days.Thursday); // 9
+// console.log(Days["Friday"]); //10
+// console.log(Days[10]); // Friday, Accessing Member
+
+
+// enum Color {
+//     Red, 
+//     Green, 
+//     Blue
+// };//starts with 0
+
+// var c: Color = Color.Green;
+// console.log(c);
+
+
+// enum Color5 {Red = 1, Green, Blue};
+
+// var colorName: string = Color5[2];
+// console.log(colorName); // Green
+
+
+// enum Color2 {
+//     Red = 1, 
+//     Green = 2, 
+//     Blue = 4
+
+// }; //can assign values to all
+
+// var colorIndex = Color2["Blue"]; // Accesing value
+// console.log(colorIndex);  // 4
+
