80f13dca529d49ddb8199bf98197ef8e89313a81
zahir9048 <zahir9048@gmail.com>
2024-07-17 19:59:48 +0500

    commit

---
+++ TypeScript/Sir Zia Repo Practice/Step 9a/main.ts
@@ -0,0 +1,94 @@
+//Note: All parameters are required
+
+//Named function
+// function add(x: number, y: number): number {
+    
+//     return x+y;
+// }
+
+
+// console.log(add(1, 3));
+
+
+
+
+
+
+
+// //Anonymous function
+// let myAdd1 = function(x: number, y: number): number { 
+// 				return x+y; 
+// };
+
+// console.log(myAdd1(3, 5));
+
+
+
+
+
+
+
+
+// //Anonymous function with explict type
+// let myAdd2: (xyz:number, abc:number)=>number = 	function(x: number, y: number): number { 
+// 												return x+y; 
+// 											};
+
+
+
+
+
+
+
+
+//type names dont matter
+// let myAdd3: (baseValue:number, increment:number)=>number = 	function(x: number, y: number): number { 
+// 																return x+y; 
+// 															};
+
+
+
+
+
+
+// // Lambda functions (Arrow Function)		 													
+// let myAdd4 = (a : number, b : number) => a + b; // short syntax
+
+
+// output will be:
+
+// var myAdd4 = function(a : number, b : number):number {return a + b};
+
+
+
+
+
+
+
+
+// type GreetFunction = () => void;
+
+// function test(){
+
+// }
+
+// function greeter(fn: GreetFunction) {
+	
+//     fn();
+// }
+
+// greeter(test);
+
+
+// callback function
+
+// function abc(fn:()=>void){
+//     console.log("inside abc func");
+//     fn();
+// }
+
+// function xyz():void{
+//     console.log("inside xyz func");
+// }
+
+// abc(xyz);
---
+++ TypeScript/Sir Zia Repo Practice/Step 9b/main.js
@@ -0,0 +1,17 @@
+//Named function with optional parameters
+// function buildName(firstName: string, lastName?: string) : string {
+//     if (lastName)
+//         return firstName + " " + lastName;
+//     else
+//         return firstName;
+// }
+// let result1 = buildName("Bob");  //ok
+// let result2 = buildName("Bob", "Adams", "Sr.");  //error, too many parameters
+// var result3 = buildName("Bob", "Adams");  //also ok
+//anonymous function type with optional parameters
+var buildName1 = function (firstName, lastName) {
+    if (lastName)
+        return firstName + " " + lastName;
+    else
+        return firstName;
+};
